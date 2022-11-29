396bacc167eec1ae6d5733c8ef408ff4487f4f3f
piotr wiktorczyk <piotrwiktor31@interia.pl>
2022-11-29 17:39:02 +0100

    done

--- zad3/clean.sh
+++ zad3/clean.sh
@@ -1,1 +1,2 @@
 docker rm --force $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
--- zad3/start.sh
+++ zad3/start.sh
@@ -1,2 +1,3 @@
-docker compose -f ./docker-compose.yaml -f ./docker-compose-dev.yaml -p todo-dev up -d
-docker compose -f ./docker-compose.yaml -f ./docker-compose-test.yaml -p todo-test up -d
+docker compose -f ./docker-compose.yml -p todo up -d
+sleep 5
+docker compose -f ./docker-compose-test.yml -p todo-test up -d
